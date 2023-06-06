fef15993ab08f4aafebdba8ba40c0fecbce2936f
沫烬染 <3341588907@qq.com>
2023-06-06 23:40:50 +0800

    更新插件，适配BetterQQNT的更改

--- renderer.js
+++ renderer.js
@@ -2,10 +2,10 @@
     const element = document.createElement("style");
     document.head.appendChild(element);
 
-    const plugins_data = betterQQNT.path.plugins_data;
+    const plugins_data = betterQQNT.plugins.test_theme.path.data;
     document.documentElement.style.setProperty(
         "--test-theme-background",
-        `url("/${plugins_data.replaceAll("\\", "/")}/test-theme/bg.jpg")`
+        `url("/${plugins_data.replaceAll("\\", "/")}/bg.jpg")`
     );
 
     test_theme.updateStyle((event, message) => {
