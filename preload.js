cbf2a015e068335fcba1fc5cc7d4fc0023e9d83c
huan_kong <huank0ng@foxmail.com>
2024-07-08 15:28:58 +0800

    [-] 刪除文件

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('HK_theme', {
-  updateStyle: callback => ipcRenderer.on('LiteLoader.HK_theme.updateStyle', callback),
-  rendererReady: () => ipcRenderer.send('LiteLoader.HK_theme.rendererReady')
-})
